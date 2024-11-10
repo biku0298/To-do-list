6bbe125c1c6dba0ebb520d61db430d46c92c9b34
biku0298 <77164059+biku0298@users.noreply.github.com>
2024-11-10 20:45:07 +0530

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -118,7 +118,8 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
